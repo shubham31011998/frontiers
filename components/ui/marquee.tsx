778b171d28e27734ac5ca0eed0374f3737f1f767
shubhamy <shubhamy@ohumhealthcare.com>
2024-09-04 12:06:50 +0530

    Fix childern type in marquee tag

--- components/ui/marquee.tsx
+++ components/ui/marquee.tsx
@@ -1,7 +1,10 @@
 // components/Marquee.js
 import React from 'react';
 
-const Marquee = ({ children, speed }) => {
+const Marquee = ({ children, speed }: Readonly<{
+    children: React.ReactNode;
+    speed:Number;
+  }>) => {
   return (
     <div className="marquee-container">
       <div className="marquee" style={{ animationDuration: `${speed}s` }}>
